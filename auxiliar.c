e6e1d2fcb2928a8cd2c0e245449a58d1582935c7
David Santos <davidccb@live.com>
2021-12-02 16:56:30 -0300

    coverter cpf

---
+++ auxiliar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "util.h"
+
+// Valida CPF
+
+    //faz os testes do cpf
+
+
+
+
+
+// Valida CNPJ
+
+char validaCnpj(char cnpj){
+    
+    
+}
+
+// Valida Codigo de Barras
+
+// Valida Nome
+
+// Valida Quantidade
+
